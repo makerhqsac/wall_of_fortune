c63f127de4483bd0640c0d16cf14428486d45ca3
Mike Machado <mike@machadolab.com>
2018-08-10 14:37:28 -0700

    color match dev

--- color_match/color_match.py
+++ color_match/color_match.py
@@ -13,21 +13,43 @@
 ###############################################################################
 
 
-
 import time
 from neopixel import *
 import argparse
+import gpiozero
+import random
+
+SWITCH_RBIT1        = 22   # pin for red bit 1 (1 on RetroHAT)
+SWITCH_RBIT2        = 23   # pin for red bit 2 (2 on RetroHAT)
+SWITCH_RBIT3        = 27   # pin for red bit 3 (3 on RetroHAT)
+SWITCH_GBIT1        =  6   # pin for green bit 1 (START on RetroHAT)
+SWITCH_GBIT2        =  5   # pin for green bit 2 (SELECT on RetroHAT)
+SWITCH_GBIT3        = 12   # pin for green bit 3 (L_SHLDR on RetroHAT)
+SWITCH_BBIT1        = 13   # pin for blue bit 1 (R_SHLDR on RetroHAT)
+SWITCH_BBIT2        = 20   # pin for blue bit 2 (X on RetroHAT)
+
 
 # LED strip configuration:
-LED_COUNT      = 16      # Number of LED pixels.
-LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_COUNT      = 2       # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0). (Y on RetroHAT)
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
+PIXEL_CURRENT   = 0      # LED number of current color
+PIXEL_TARGET    = 1      # LED number of target color
+
+
+btn_rbit1 = gpiozero.Button(SWITCH_RBIT1, pull_up=True)
+btn_rbit2 = gpiozero.Button(SWITCH_RBIT2, pull_up=True)
+btn_rbit3 = gpiozero.Button(SWITCH_RBIT3, pull_up=True)
+btn_gbit1 = gpiozero.Button(SWITCH_GBIT1, pull_up=True)
+btn_gbit2 = gpiozero.Button(SWITCH_GBIT2, pull_up=True)
+btn_gbit3 = gpiozero.Button(SWITCH_GBIT3, pull_up=True)
+btn_bbit1 = gpiozero.Button(SWITCH_BBIT1, pull_up=True)
+btn_bbit2 = gpiozero.Button(SWITCH_BBIT2, pull_up=True)
 
 
 # Define functions which animate LEDs in various ways.
@@ -38,89 +60,67 @@ def colorWipe(strip, color, wait_ms=50):
         strip.show()
         time.sleep(wait_ms/1000.0)
 
-def theaterChase(strip, color, wait_ms=50, iterations=10):
-    """Movie theater light style chaser animation."""
-    for j in range(iterations):
-        for q in range(3):
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, color)
-            strip.show()
-            time.sleep(wait_ms/1000.0)
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, 0)
+def readSwitches():
+    rbyte = 0
+    gbyte = 0
+    bbyte = 0
 
-def wheel(pos):
-    """Generate rainbow colors across 0-255 positions."""
-    if pos < 85:
-        return Color(pos * 3, 255 - pos * 3, 0)
-    elif pos < 170:
-        pos -= 85
-        return Color(255 - pos * 3, 0, pos * 3)
-    else:
-        pos -= 170
-        return Color(0, pos * 3, 255 - pos * 3)
+    # FIXME: use LS bits or MS bits of each color byte for best color?
+    rbyte |= btn_rbit1.value << 2
+    rbyte |= btn_rbit2.value << 1
+    rbyte |= btn_rbit3.value << 0
 
-def rainbow(strip, wait_ms=20, iterations=1):
-    """Draw rainbow that fades across all pixels at once."""
-    for j in range(256*iterations):
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, wheel((i+j) & 255))
-        strip.show()
-        time.sleep(wait_ms/1000.0)
+    gbyte |= btn_gbit1.value << 2
+    gbyte |= btn_gbit2.value << 1
+    gbyte |= btn_gbit3.value << 0
 
-def rainbowCycle(strip, wait_ms=20, iterations=5):
-    """Draw rainbow that uniformly distributes itself across all pixels."""
-    for j in range(256*iterations):
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
-        strip.show()
-        time.sleep(wait_ms/1000.0)
+    bbyte |= btn_bbit1.value << 1
+    bbyte |= btn_bbit2.value << 0
 
-def theaterChaseRainbow(strip, wait_ms=50):
-    """Rainbow movie theater light style chaser animation."""
-    for j in range(256):
-        for q in range(3):
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, wheel((i+j) % 255))
-            strip.show()
-            time.sleep(wait_ms/1000.0)
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, 0)
+    return (rbyte << 16) | (gbyte << 8) | bbyte
 
-# Main program logic follows:
-if __name__ == '__main__':
+
+def run_main():
     # Process arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    # parser.add_argument('-t', '--target', action='store_true', help='set specific target color')
     args = parser.parse_args()
 
-    # Create NeoPixel object with appropriate configuration.
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-    # Intialize the library (must be called once before other functions).
     strip.begin()
 
-    print ('Press Ctrl-C to quit.')
+    print('Press Ctrl-C to quit.')
     if not args.clear:
         print('Use "-c" argument to clear LEDs on exit')
 
     try:
 
+        # TODO: implement color override (useful for testing)
+        target_bits = random.randint(1, 256)
+        target_r = target_bits >> 5
+        target_r &= 0x07
+        target_g = target_bits >> 2
+        target_g &= 0x07
+        target_b = target_bits
+        target_b &= 0x03
+        target_color = Color(target_r, target_g, target_b)
+        strip.setPixelColor(PIXEL_TARGET, target_color)
+
         while True:
-            print ('Color wipe animations.')
-            colorWipe(strip, Color(255, 0, 0))  # Red wipe
-            colorWipe(strip, Color(0, 255, 0))  # Blue wipe
-            colorWipe(strip, Color(0, 0, 255))  # Green wipe
-            print ('Theater chase animations.')
-            theaterChase(strip, Color(127, 127, 127))  # White theater chase
-            theaterChase(strip, Color(127,   0,   0))  # Red theater chase
-            theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
-            print ('Rainbow animations.')
-            rainbow(strip)
-            rainbowCycle(strip)
-            theaterChaseRainbow(strip)
+            switch_color = readSwitches()
+            strip.setPixelColor(PIXEL_CURRENT, switch_color)
+            time.sleep(0.05)
+
+            if switch_color == target_color:
+                print("WINNER WINNER!")
+                return
 
     except KeyboardInterrupt:
         if args.clear:
             colorWipe(strip, Color(0,0,0), 10)
 
 
+# Main program logic follows:
+if __name__ == '__main__':
+    run_main()
