27b8305f5e25a6cc24db7d1833b662fde673a52d
Richard Julian <richard@rjulian.net>
2018-08-19 15:57:24 -0700

    Add extra logic for calculating when hal speaks

--- hal/hal_switch_led.py
+++ hal/hal_switch_led.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import time
+import datetime
 from neopixel import *
 from gpiozero import Button
 import argparse
@@ -17,6 +18,10 @@ LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 button1 = Button(14)
 button2 = Button(15)
 
+buttons = [ button1, button2 ]
+speech_timer = datetime.datetime.now()
+SPEECH_DELAY = 60
+
 # Define functions which animate LEDs in various ways.
 
 def set_red(strip, value=200):
@@ -25,31 +30,49 @@ def set_red(strip, value=200):
             strip.setPixelColor(i, Color(0,value,0))
     strip.show()
 
+def number_of_buttons_pressed():
+    """Count of buttons pressed at one time """
+    buttons_pressed = 0
+    for button in buttons:
+        if button.is_pressed:
+            buttons_pressed += 1
+    return buttons_pressed
 
-# Main program logic follows:
+def too_soon_to_speak():
+    """ Check if enough time has elapsed since last spoken """
+    now_time = datetime.datetime.now()
+    if int((speech_timer - now_time).total_seconds()) > SPEECH_DELAY:
+        speech_timer = now_time
+        return False
+    else:
+        return True
+
+def speak_audio(current_buttons)
+    """ Checks if hal can speak, if it can play wave file based on number of buttons pressed """
+    if not too_soon_to_speak():
+        if current_buttons > 1:
+            print("I don't like what you're doing, Dave.")
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
     args = parser.parse_args()
-
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     strip.begin()
-
     print ('Press Ctrl-C to quit.')
     if not args.clear:
         print('Use "-c" argument to clear LEDs on exit')
-
     try:
-
         while True:
-
+            current_buttons = number_of_buttons_pressed()
+            set_red(strip, (250 - (current_buttons * 30)))
+            speak_audio(current_buttons)
             if button1.is_pressed and button2.is_pressed:
                 set_red(strip,120)
             elif button1.is_pressed or button2.is_pressed:
                 set_red(strip, 180)
             else:
                 set_red(strip, 250)
-
     except KeyboardInterrupt:
         if args.clear:
             colorWipe(strip, Color(0,0,0), 10)
