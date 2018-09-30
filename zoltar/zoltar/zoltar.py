22d9b7b7d6980ddd427678092310d7d1a1c4bebd
Richard Julian <richard@rjulian.net>
2018-09-30 14:25:13 -0700

    Add code for eyes being on and off switch to looping over servo values

--- zoltar/zoltar/zoltar.py
+++ zoltar/zoltar/zoltar.py
@@ -1,41 +1,66 @@
-from gpiozero import AngularServo, Button
+from gpiozero import AngularServo, Button, LED
 from time import sleep
 
 SERVO_GPIO          = 19
 SERVO_MIN_ANGLE     = 0
 SERVO_MAX_ANGLE     = 179
-SERVO_INITIAL_ANGLE = 90
+SERVO_INITIAL_ANGLE = 179
 
 BUTTON_GPIO         = 5
+LED1_GPIO           = 17
+LED2_GPIO           = 27
+
 
 class Zoltar(object):
     def __init__(self):
         print('Finally, a Zoltar!')
         self.is_moving = False
+        self.left_eye = LED(LED1_GPIO)
+        self.right_eye = LED(LED2_GPIO)
 
     def begin_moving(self):
+        self.left_eye.on()
+        self.right_eye.on()
         servo = AngularServo(SERVO_GPIO,
                      min_angle=SERVO_MIN_ANGLE,
                      max_angle=SERVO_MAX_ANGLE,
                      initial_angle=SERVO_INITIAL_ANGLE)
-        print('We are moving')
         while self.is_moving:
-            servo.angle = 0
-            print("0")
-            sleep(2.5)
-            servo.angle = 90
-            print("90")
-            sleep(1)
-            servo.angle = 135
-            print("135")
-            sleep(0.5)
-            servo.angle = 179
-            print("179")
-            sleep(1)
+            for x in reversed(range(0, 179)):
+                servo.angle = x
+                sleep(0.01)
+            for x in range(0, 179):
+                servo.angle = x
+                if servo.angle == 0 :
+                    sleep(0.5)
+                else:
+                    sleep(0.01)
+        self.finale()
+
+    def eyes_off(self):
+        self.right_eye.off()
+        self.left_eye.off()
+
+    def eyes_on(self):
+        self.left_eye.on()
+        self.right_eye.on()
+
+    def finale(self):
+        self.eyes_off()
+        sleep(1)
+        self.eyes_on()
+        sleep(1)
+        self.eyes_off()
+        sleep(1)
+        self.eyes_on()
+        sleep(1)
+        self.eyes_off()
 
     def stop_moving(self):
         print('Button detected')
         self.is_moving = False
+        self.right_eye.off()
+        self.left_eye.off()
 
 
 if __name__ == "__main__":
