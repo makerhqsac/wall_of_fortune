e0a5c187f1b7936413e5b2f359198320cbb80f86
Richard Julian <richard@rjulian.net>
2018-10-04 17:56:45 -0700

    Massive overhaul of xychase code to use gpiozero

--- xy_chase/xy_chase.py
+++ xy_chase/xy_chase.py
@@ -18,22 +18,16 @@
 # Import required libraries
 import time
 import datetime
-import RPi.GPIO as GPIO
-import spidev
-
-HALL_PIN = 17
-LEDS = [25, 5, 6, 12, 13, 19, 26]
-
-current = 0
+from gpiozero import LED, Button
 
 ROUTES = [
-[0,1,2],
-[1,2,3],
-[2,3,4],
-[3,4,5],
-[4,5,6],
-[5,6,0],
-[6,0,1]
+['NewAmsterdam','Lima','BuenosAires'],
+['Lima','BuenosAires','London'],
+['BuenosAires','London','Venice'],
+['London','Venice','Capetown'],
+['Venice','Capetown','Ceylon'],
+['Capetown','Ceylon','NewAmsterdam'],
+['Ceylon','NewAmsterdam','Lima']
 ];
 
 LED_MAPPING = {
@@ -46,118 +40,160 @@ LED_MAPPING = {
         26 : 'Ceylon'
         }
 
+HALL_MAPPING = {
+        4 : 'NewAmsterdam',
+        17 : 'Lima',
+        18 : 'BuenosAires',
+        27 : 'London',
+        22 : 'Venice',
+        23 : 'Capetown',
+        24 : 'Ceylon'
+        }
+
 ROUTELENGTH = 3
 ROUTES = 5
-current_route = 0
-route_index = 0
-timeout = 60
+TIMEOUT = 60
 threshold = 3
 
-spi = spidev.SpiDev()
-spi.open(0,0)
-spi.max_speed_hz = 1000000
+def read_introduction():
+    print('You are the captain')
 
-# Tell GPIO library to use GPIO references
-GPIO.setmode(GPIO.BCM)
-
-# Set Switch GPIO as input
-# Pull high by default
-GPIO.setup(HALL_PIN , GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-for l in LEDS:
-  GPIO.setup(l, GPIO.OUT)
-GPIO.add_event_detect(HALL_PIN, GPIO.BOTH, callback=sensor_callback, bouncetime=200)
-
-
-# Function to read SPI data from MCP3008 chip
-# Channel must be an integer 0-7
-def read_channel(channel):
-  adc = spi.xfer2([1,(8+channel)<<4,0])
-  data = ((adc[1]&3) << 8) + adc[2]
-  return data
-
-# Function to convert data to voltage level,
-# rounded to specified number of decimal places.
-def convert_volts(data,places):
-  volts = (data * 3.3) / float(1023)
-  volts = round(volts,places)
-  return volts
-
-def sensor_callback(channel):
-  # Called if sensor output changes
-  timestamp = time.time()
-  stamp = datetime.datetime.fromtimestamp(timestamp).strftime('%H:%M:%S')
-  if GPIO.input(channel):
-    # No magnet
-    print("Sensor HIGH " + stamp + " " + str(current))
-  else:
-    # Magnet
-    print("Sensor LOW " + stamp)
-    next_location()
-
-def next_location():
-  route_index += 1
-  # Do game logic here. Advance to next location or win the level.
-  if route_index < ROUTELENGTH :
-    current = ROUTES[current_route][route_index]
-    clear_leds()
-    set_led_on(current)
-  else:
-    winner()
-
-def winner():
-    print("winner!")
-    current_route += 1
-    route_index = 0
-    if current_route >= 5:
-      current_route = 0
-
-def loser():
-    print("loser!")
-    current_route += 1
-    route_index = 0
-    if current_route >= 5:
-      current_route = 0
-
-def clear_leds():
-  for l in LEDS:
-    GPIO.output(l,GPIO.LOW)
-
-def set_led_on(channel):
-  GPIO.output(LEDS[channel],GPIO.HIGH)
+class XyChase(object):
+    def __init__(self):
+        print('A new chase begins')
+        initiate_buttons()
+        initiate_leds()
+        create_mapping()
+        self.routes_completed = 0
+        self.current_route = 0
+        self.current_destination = 0
+        self.route_status = [ 'Uninitiated', 'Uninitiated', 'Uninitiated' ]
 
 
+    def initiate_buttons(self):
+        self.button1 = Button(4)
+        self.button2 = Button(17)
+        self.button3 = Button(18)
+        self.button4 = Button(27)
+        self.button5 = Button(22)
+        self.button6 = Button(23)
+        self.button7 = Button(24)
+
+    def initiate_leds(self):
+        self.led1 = LED(25)
+        self.led2 = LED(5)
+        self.led3 = LED(6)
+        self.led4 = LED(12)
+        self.led5 = LED(13)
+        self.led6 = LED(19)
+        self.led7 = LED(26)
+
+    def cleanup_hardware(self):
+        self.close_buttons()
+        self.close_leds()
+
+    def close_buttons(self):
+        self.button1.close()
+        self.button2.close()
+        self.button3.close()
+        self.button4.close()
+        self.button5.close()
+        self.button6.close()
+        self.button7.close()
+
+    def close_leds(self):
+        self.led1.close()
+        self.led2.close()
+        self.led3.close()
+        self.led4.close()
+        self.led5.close()
+        self.led6.close()
+        self.led7.close()
+
+    def create_mapping(self):
+        self.mapping = {
+                'NewAmsterdam' : [self.button1, self.led1],
+                'Lima' : [self.button2, self.led2],
+                'BuenosAires' : [self.button3, self.led3],
+                'London' : [self.button4, self.led4],
+                'Venice' : [self.button5, self.led5],
+                'Capetown' : [self.button6, self.led6],
+                'Ceylon' : [self.button7, self.led7]
+                }
+
+    def next_destination(self):
+        destination = ROUTES[self.current_route][self.current_destination]
+        self.route_status[self.current_destination] = 'Completed'
+        self.mapping[destination][0].when_pressed = None
+        self.mapping[destination][1].off() = None
+        self.current_destination += 1
+
+    def pickup_cargo(self):
+        destination = ROUTES[self.current_route][self.current_destination]
+        print('Pick up cargo at', destination)
+        self.mapping[destination][0].when_pressed = self.next_destination
+        sleep(0.5)
+        self.mapping[destination][1].on()
+        self.route_status[self.current_destination] = 'InProgress'
+
+    def safe_harbor(self):
+        destination = ROUTES[self.current_route][self.current_destination]
+        self.deliver_next_cargo()
+        print('There is a storm at sea.')
+        print('Take safe harbor at ', destination)
+        self.mapping[destination][0].when_pressed = self.next_destination
+        sleep(0.5)
+        self.mapping[destination][1].on()
+        self.route_status[self.current_destination] = 'InProgress'
+
+    def deliver_cargo(self):
+        destination = ROUTES[self.current_route][self.current_destination]
+        print('The seas have calmed')
+        print('Deliver your cargo to ', destination)
+        self.mapping[destination][0].when_pressed = self.next_destination
+        sleep(0.5)
+        self.mapping[destination][1].on()
+        self.route_status[self.current_destination] = 'InProgress'
+
+    def deliver_next_cargo(self):
+        destination = ROUTES[self.current_route + 1][self.current_destination]
+        print('Deliver your cargo to ', destination)
+
+    def check_game_status(self):
+        if self.route_status[self.current_destination] == 'Uninitiated':
+            route_state = self.route_status[self.current_destination]
+            if self.current_destination == 0 and route_state != 'InProgress':
+                self.pickup_cargo()
+            if self.current_destination == 1 and route_state != 'InProgress':
+                self.safe_harbor()
+            if self.current_destination == 2 and route_state != 'InProgress':
+                self.deliver_cargo()
+
+    def lose_game(self):
+        print('Oh no, you lost')
+        self.cleanup_hardware()
+
+    def win_game(self):
+        print('You won, congrats!')
+        self.cleanup_hardware()
+
+    def begin_game(self):
+        read_introduction()
+        self.start_time = time.time()
+        while True:
+            if time.time() - self.start_time > TIMEOUT:
+                self.lose_game()
+                break
+            if self.current_destination = 3:
+                self.win_game()
+                break
+            self.check_game_status()
 
 def main():
-  # Wrap main content in a try block so we can
-  # catch the user pressing CTRL-C and run the
-  # GPIO cleanup function. This will also prevent
-  # the user seeing lots of unnecessary error
-  # messages.
-
-  current = 0
-
-  # Get initial reading
-  sensor_callback(HALL_PIN)
-
-  try:
-    start_time = time.time()
-    # Loop until users quits with CTRL-C
-    clear_leds()
-    set_led_on(current)
     while True :
-      mag_level = read_channel(current)
-      mag_volts = convert_volts(mag_level,2)
-      if (mag_volts > threshold):
-        next_location()
-        clear_leds()
-        set_led_on(current)
-      if (time.time() - start_time > timeout):
-        loser()
-
-  except KeyboardInterrupt:
-    # Reset GPIO settings
-    GPIO.cleanup()
+        chase = XyChase()
+        chase.begin_game()
+        sleep(10)
 
 if __name__=="__main__":
    main()
